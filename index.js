508f0ff72a187b03422eba2f2309fdf6ff5950d0
cdnss <32522595+cdnss@users.noreply.github.com>
2024-12-05 22:11:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // index.js
 const chromium = require('chrome-aws-lambda');
 const puppeteer = require('puppeteer-core');
-async function handler(event, context) => {
+async function handler(event, context){
     const browser = await puppeteer.launch({
         args: chromium.args,
         defaultViewport: chromium.defaultViewport,
